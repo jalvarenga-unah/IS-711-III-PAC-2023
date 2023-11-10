d60c005ffdfccdc2e736ac12cb7cee8308879372
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2023-11-09 20:49:04 -0600

    Trabajando con funciones asincronas

--- principios-js/js/fetch.js
+++ principios-js/js/fetch.js
@@ -34,23 +34,93 @@ function getData() {
             return response.json() // * retorna una promesa
         })
         .then((data) => {
-            console.log(data)
 
+            // ul
+            // const ul = document.querySelector('#resultados')
+            const ul = document.createElement('ul')
+            const section = document.querySelector('section')
 
+            section.appendChild(ul)
 
+            data.forEach(user => {
+
+                const li = document.createElement('li')
+                const { name, email } = user
+
+                li.textContent = `${name} - ${email}` // * template string
+
+                ul.appendChild(li)
+
+            });
 
         }).catch((error) => {
             console.log(error)
         })
 }
 
+
+//definiendo una función asíncrona
+async function getInfo() {
+
+    try {
+        // resolvemos la promesa, y detiene la ejecución de la función
+        const response = await fetch('https://jsonplaceholder.typicode.com/users') // Response (promesa)
+
+        if (!response.ok) {
+            throw Error(response.status + ' ' + response.statusText)
+        }
+
+        return await response.json() // [ {....}, {....}, {....} ]
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 // const boton_datos = document.getElementById('btnGetData')
 const cargarDatos = document.querySelector('#btnGetData')
-const lista = document.querySelector('#lista')
+
 // const botones = document.querySelectorAll('button')
 
+let users = [] //global
 
 // asignar un evento al boton
-cargarDatos.addEventListener('click', getData)
+cargarDatos.addEventListener('click', async function () {
+    users = []
+    users = await getInfo()
+
+    // const ul = document.createElement('ul')
+    // const section = document.querySelector('section > div')
+
+    // section.innerHTML = '' //limpiar el contenido del section
+
+    // section.appendChild(ul)
+
+    const ul = document.querySelector('#resultados')
+
+    let li = '' // String
+
+    users.forEach(user => {
+
+        // const li = document.createElement('li')
+        const { name, email, username, address } = user
+        const { city } = address
+        console.log(city)
+        // concatenar el contenido de la etiqueta li
+        li += `  <li class="list-group-item d-flex justify-content-between align-items-start">
+                    <div class="ms-2 me-auto">
+                    <div class="fw-bold">${email}</div>
+                    ${name}
+                    </div>
+                    <span class="badge bg-primary rounded-pill">${username}</span>
+                </li>
+      ` // * template string
+        // li.textContent = `${name} - ${email}` // * template string
+
+        // ul.appendChild(li)
+        ul.innerHTML = li
+
+    });
+})
 
 // })
