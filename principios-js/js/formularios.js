fe6b184dd588cc98bcffec8f5b86010ccfee8ca4
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2023-11-14 19:23:55 -0600

    introducción a los formularios

---
+++ principios-js/js/formularios.js
@@ -0,0 +1,13 @@
+
+const guardarDatos = async () => {
+    // Obtener los datos del formulario
+    // hacer validaciones
+    // enviar los datos al servidor
+    // retornar la respuesta del servidor
+    return true
+}
+
+guardarDatos()
+    .then((respuesta) => {
+        console.log(respuesta)
+    })
