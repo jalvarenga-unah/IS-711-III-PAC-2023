bfebea008c15bba0590eef53bc95c6814be12b56
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2023-11-22 20:48:37 -0600

    Creando rutas en el API

--- intro-node/server.js
+++ intro-node/server.js
@@ -1,17 +1,48 @@
 const http = require('node:http') // Paquete http para crear el servidor
+const productos = require('./productos/productos.json') // Importamos el archivo productos.json
 
-
-const server = http.createServer((request, response) => {
+const server = http.createServer((request, res) => {
 
     // * Todo esto ocurre en el servidor
     // ? request: es lo que el cliente envía al servidor
-    // * response: es lo que el servidor envía al cliente
+    // * res: es lo que el servidor envía al cliente
 
-    console.log(request.url)
-    console.log(request.method)
+    const { url, method } = request
 
-    response.writeHead(200, { 'Content-Type': 'text/html' })
-    response.end('<h1>Hola mundo desde nodeJS</h1>')
+    switch (method) {
+        case 'GET':
+            switch (url) {
+                case '/':
+                    res.statusCode = 200
+                    res.end('Hola desde el home')
+                    break;
+                case '/productos':
+                    res.writeHead(200, { 'Content-Type': 'application/json' })
+                    // TODO: realizar la lógica para consultar los productos en la base de datos
+                    res.end(JSON.stringify(productos))
+                    break;
+                case '/productos/1':
+                    res.writeHead(200, { 'Content-Type': 'application/json' })
+                    const resultado = productos.filter((producto) => producto.id === 1)
+                    res.end(JSON.stringify(resultado))
+                    break;
+                default:
+                    res.statusCode = 404
+                    res.end('page not found')
+            }
+
+            break;
+        case 'POST':
+            break;
+        case 'DELETE':
+            break;
+        case 'PUT':
+            break;
+
+        default:
+            res.statusCode = 404
+            res.end('page not found')
+    }
 })
 
 // ? el 0, es para que asigne un puerto libre
